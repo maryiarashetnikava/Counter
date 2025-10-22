8843934afe44712272999915ba8f0305736c88d8
Mary <mariaprohor75@gmail.com>
2025-10-22 15:39:11 +0200

    Task sprint 3

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    var currentCount = 0
 
-
+    @IBAction func buttonClick(_ sender: Any) {
+        currentCount += 1
+        counterLabel.text = "Значение счётчика: \(currentCount)"
+    }
+    
 }
 
